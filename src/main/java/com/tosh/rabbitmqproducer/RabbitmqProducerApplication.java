b7e9a9db2316e02708e8eb4bc1908a15db1ca392
raymond gitonga <raytosh95@gmail.com>
2020-04-07 01:10:36 +0300

    configure docker

--- src/main/java/com/tosh/rabbitmqproducer/RabbitmqProducerApplication.java
+++ src/main/java/com/tosh/rabbitmqproducer/RabbitmqProducerApplication.java
@@ -1,13 +1,25 @@
 package com.tosh.rabbitmqproducer;
 
+import com.tosh.rabbitmqproducer.producer.HelloRabbitProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-public class RabbitmqProducerApplication {
+public class RabbitmqProducerApplication  implements CommandLineRunner {
+
+	@Autowired
+	private HelloRabbitProducer helloRabbitProducer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqProducerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		helloRabbitProducer.sendHello("Tosh " + Math.random());
+	}
 }
