5905aa195b6798aba38a1f8f5d7aaedc2e289693
raymond gitonga <raytosh95@gmail.com>
2020-04-07 12:57:43 +0300

    Add fixed rate producer

--- src/main/java/com/tosh/rabbitmqproducer/RabbitmqProducerApplication.java
+++ src/main/java/com/tosh/rabbitmqproducer/RabbitmqProducerApplication.java
@@ -6,13 +6,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@EnableScheduling
 public class RabbitmqProducerApplication  implements CommandLineRunner {
 
-	@Autowired
-	private HelloRabbitProducer helloRabbitProducer;
+//	@Autowired
+////	private HelloRabbitProducer helloRabbitProducer;
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqProducerApplication.class, args);
@@ -20,6 +24,6 @@ public class RabbitmqProducerApplication  implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		helloRabbitProducer.sendHello("Tosh " + Math.random());
+//		helloRabbitProducer.sendHello("Tosh " + Math.random());
 	}
 }
